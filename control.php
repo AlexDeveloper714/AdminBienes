37d93d192e132cc7d2250c005ff643476a38cd83
AlexDeveloper714 <idiazcruz@uniminuto.edu.co>
2017-04-21 10:55:20 -0500

    arreglo PHP (revisar codigo de salida :/)

--- control.php
+++ control.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'database.php';
 $db2 = new database();
 if (isset($_POST['enviarCliente'])) {
@@ -11,19 +12,20 @@ if (isset($_POST['enviarCliente'])) {
     $destino2 = "img/" . $exten;
     copy($ruta, $destino);
     rename($destino, $destino2);
-    if ($db2->verificarIdClientes($_POST ['cedula'], "clientes")) {
-        echo "<h1>
-           Cedula ya registrada, retrocediendo...
-       </h1>";
-        echo "<script type=\"text/javascript\">
-            setTimeout(function(){ history.go(-1); },500);
-       </script>";
-        exit;
-    } else {
-        $db2->insertar(array($_POST ['cedula'], $_POST['nombre'], $_POST['apellido'],
-            $_POST['genero'], $_POST ['fecha'], $_POST['correo'], $_POST['hijos'], $destino2), "clientes");
-        require 'index.php';
-    }
+//    if ($db2->verificarIdClientes($_POST ['cedula'], "clientes")) {
+//        echo "<h1>
+//           Cedula ya registrada, retrocediendo...
+//       </h1>";
+//        echo "<script type=\"text/javascript\">
+//            setTimeout(function(){ history.go(-1); },500);
+//       </script>";
+//        exit;
+//    } else {
+//        
+//    }
+    $db2->insertar(array($_POST ['cedula'], $_POST['nombre'], $_POST['apellido'],
+        $_POST['genero'], $_POST ['fecha'], $_POST['correo'], $_POST['hijos'], $destino2), "clientes");
+    require 'index.php';
 }
 
 if (isset($_POST['enviarActivo'])) {
